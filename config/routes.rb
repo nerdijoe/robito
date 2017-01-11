63434ae8c699711260610e749bcd7fbd9a431f17
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-12 00:16:35 +0800

    routes.rb : add devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
+  devise_scope :user do
+    get '/users/auth/:provider/upgrade' => 'omniauth_callbacks#upgrade', as: :user_omniauth_upgrade
+    get '/users/auth/:provider/setup', :to => 'omniauth_callbacks#setup'
+  end
+
   root 'welcome#index'
   get 'welcome/index'
 
