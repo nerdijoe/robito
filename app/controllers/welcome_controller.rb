92b4b300864e0a65530d82fa93dd8f4245334886
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-12 01:22:24 +0800

    manage to display user recent media

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
+
+
+
   def index
+    @client = Instagram.client(access_token: current_user.instagram.accesstoken)
+    @recent = @client.user_recent_media
   end
 end
