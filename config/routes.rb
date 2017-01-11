e3ab061a3c0b82ad2b742de7f020734e9df04486
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-12 00:05:53 +0800

    add RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-  
+  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+
   root 'welcome#index'
   get 'welcome/index'
 
