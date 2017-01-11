7df4cb68b1fa9de05c6a2656cf0284f714631b95
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-12 00:04:22 +0800

    add OmniauthCallbacksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, class_name: 'FormUser'
+  devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  
   root 'welcome#index'
   get 'welcome/index'
 
