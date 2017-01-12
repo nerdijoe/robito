270535ac83f23541bd357449c14b7cd5314b427d
Nabil <nabilalsouhily@gmail.com>
2017-01-12 16:22:34 +0800

    Adding The campaigns table

--- db/migrate/20170112081200_create_campaigns.rb
+++ db/migrate/20170112081200_create_campaigns.rb
@@ -1,7 +1,15 @@
 class CreateCampaigns < ActiveRecord::Migration
   def change
     create_table :campaigns do |t|
-
+    	t.text :description
+    	t.string :product_category
+    	t.string :product_name
+    	t.string :company_name
+    	t.string :email
+    	t.integer :phone_number
+    	t.string :address
+    	t.string :link
+    	t.json   :image 
       t.timestamps null: false
     end
   end
