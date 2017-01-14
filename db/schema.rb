c43264d10d8e4990ed0586ee4169e947a72ad09c
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-14 20:56:37 +0800

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170114041605) do
-
+ActiveRecord::Schema.define(version: 20170114103149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +37,11 @@ ActiveRecord::Schema.define(version: 20170114041605) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "brand_id"
+    t.string   "tag"
+    t.string   "location"
+    t.integer  "age"
+    t.integer  "gender"
+    t.string   "interests"
   end
 
   add_index "campaigns", ["brand_id"], name: "index_campaigns_on_brand_id", using: :btree
